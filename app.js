8c7d15ca75b60b6cb5b825b37bf8f09569ee8ef6
PedroMMercado <pedrommercado@gmail.com>
2015-09-23 06:21:09 -0400

    a

--- app.js
+++ app.js
@@ -26,8 +26,6 @@ app.controller('mainController',['$scope','$resource','$http',function($scope,$r
         $scope.data = data;
         console.log(data);
     });
-    
-    //http://api.nytimes.com/svc/search/v2/articlesearch.json?fq=romney&facet_field=day_of_week&begin_date=20120101&end_date=20120101&api-key=18dedb9a95e1ba40cb00fef7f9020f92:0:73004522
 }]);
 
 app.controller('resultController',['$scope',function($scope){
